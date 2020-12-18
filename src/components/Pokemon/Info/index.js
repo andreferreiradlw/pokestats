8008156ae5de26be8f15353fe71d2170bd70f6bd
andreferreiradlw <ferreirandre25@gmail.com>
2020-12-18 18:02:36 +0000

    Pokemon page initial Info section

--- src/components/Pokemon/Info/index.js
+++ src/components/Pokemon/Info/index.js
@@ -2,35 +2,99 @@ import { useSelector } from 'react-redux'
 // helpers
 import { capitalize } from '../../../helpers/typography'
 // components
-import { Name, Id, ImageContainer, Image } from './StyledInfo'
+import Loading from '../../Loading'
+import {
+  Name,
+  ImageContainer,
+  Image,
+  Genera,
+  Flavor,
+  DescriptionList,
+} from './StyledInfo'
 import Box from '../../Box'
 
 export default function Info() {
   // pokemon info
   const pokemonInfo = useSelector((state) => state.pokemon.info)
+  // biology
+  const pokemonBio = useSelector((state) => state.pokemon.biology)
+  // game version
+  const gameVersion = useSelector((state) => state.game.version)
 
-  const { abilities, id, name } = pokemonInfo.data
+  // data
+  const { types, abilities, id, name, weight, height } = pokemonInfo.data
+  const { genera, flavor_text_entries } = pokemonBio.data
+
+  // flavor text
+  const flavorText = (version) => {
+    const versionEntry = flavor_text_entries.filter((entry) => {
+      return entry.version.name === version
+    })
+    // return text
+    return versionEntry[0].flavor_text
+  }
+
+  // decimal number
+  const insertDecimal = (num) => {
+    return num / 10
+  }
 
   return (
-    <Box as="section" width="100%" noGutter>
-      <Box direction="row" align="flex-end" justify="center" margin="2rem 0">
-        <Name>{capitalize(name)}</Name>
-        <Id>#{id}</Id>
-      </Box>
-      <Box direction={{ xxs: 'column', md: 'row' }} noGutter width="100%">
-        <ImageContainer sizes={{ xxs: 12, md: 6 }} justify="flex-end" debug>
+    <Box as="section" noGutter margin="2rem 0">
+      <Box
+        direction={{ xxs: 'column', lg: 'row' }}
+        align={{ xxs: 'center', lg: 'stretch' }}
+        noGutter
+      >
+        <ImageContainer sizes={{ xxs: 12, lg: 6 }} justify="flex-end" noGutter>
           <Image
             src={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`}
           />
         </ImageContainer>
-        <Box
-          sizes={{ xxs: 12, md: 6 }}
-          direction={{ xxs: 'column', sm: 'row' }}
-        >
-          <Box>Pokedex Data</Box>
-          <Box>Base Stats</Box>
+        <Box sizes={{ xxs: 12, lg: 6 }} noGutter>
+          <Box grow={0}>
+            <Name>{capitalize(name)}</Name>
+          </Box>
+          {pokemonBio.isLoading ? (
+            <Loading />
+          ) : (
+            <Box justify="flex-start" align="flex-start">
+              <Genera>{genera}</Genera>
+              <Flavor>{gameVersion && flavorText(gameVersion)}</Flavor>
+              <DescriptionList
+                forwardedAs="dl"
+                direction="row"
+                align="flex-start"
+                flexWrap="wrap"
+                grow={0}
+                noGutter
+              >
+                <dt>National ID</dt>
+                <dd>{`#${id}`}</dd>
+                <dt>Category</dt>
+                <dd>{genera}</dd>
+                <dt>Weight</dt>
+                <dd>{`${insertDecimal(weight)} kg`}</dd>
+                <dt>Height</dt>
+                <dd>{`${insertDecimal(height)} m`}</dd>
+                <dt>Abilities</dt>
+                <dd>
+                  {abilities.map(({ ability }, i) => {
+                    return <span key={i}>{capitalize(ability.name)}</span>
+                  })}
+                </dd>
+                <dt>Types</dt>
+                <dd>
+                  {types.map(({ type }, i) => {
+                    return <span key={i}>{capitalize(type.name)}</span>
+                  })}
+                </dd>
+              </DescriptionList>
+            </Box>
+          )}
         </Box>
       </Box>
+      <Box>Base Stats</Box>
     </Box>
   )
 }

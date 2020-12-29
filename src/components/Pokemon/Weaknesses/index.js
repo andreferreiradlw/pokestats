5508f9e5f40832f2d64e05e39055a1eabcb6f526
andreferreiradlw <ferreirandre25@gmail.com>
2020-12-29 15:33:25 +0000

    Weakness State Update by Type Requests

--- src/components/Pokemon/Weaknesses/index.js
+++ src/components/Pokemon/Weaknesses/index.js
@@ -9,30 +9,68 @@ import { SectionTitle, Table, Numbered } from '../StyledPokemon'
 
 export default function Weaknesses({ ...rest }) {
   // pokemon info
-  const pokemonInfo = useSelector((state) => state.pokemon.info)
+  const pokemonInfo = useSelector(state => state.pokemon.info)
   // data
   const { types } = pokemonInfo.data
 
-  // fetch type data
+  // loading state
+  const [typeLoading, setTypeLoading] = useState(true)
+  // weakness initial state
+  const [weakness, setWeakness] = useState({
+    double_damage_from: [],
+    double_damage_to: [],
+    half_damage_from: [],
+    half_damage_to: [],
+    no_damage_from: [],
+    no_damage_to: [],
+  })
+
+  // fetch type data on load
   useEffect(() => {
     // requests array
     const axiosRequests = []
 
-    const fetchTypeData = (requests) => {
+    const fetchTypeData = requests => {
       axios
         .all(requests)
         .then(
           axios.spread((...responses) => {
-            console.log('responses', responses)
+            responses
+              .map(response => {
+                // damage relation for each type
+                return response.data.damage_relations
+              })
+              .forEach(type => {
+                // copy old state
+                const newWeakness = { ...weakness }
+                // for each type, loop relation array
+                Object.keys(type).forEach(key => {
+                  type[key].forEach(relation => {
+                    // check if type already exists in state key
+                    if (weakness[key].indexOf(relation.name) === -1) {
+                      // if it doesn't, change value
+                      newWeakness[key].push(relation.name)
+                    }
+                  })
+                })
+                // set new state
+                setWeakness(newWeakness)
+              })
           })
         )
-        .catch((errors) => {
-          // react on errors.
-          console.log('error', errors)
+        .catch(error => {
+          // catch errors
+          console.log('error', error)
         })
+        .finally(
+          // change loading state
+          setTypeLoading(false)
+        )
     }
 
     if (types.length) {
+      // create an axios request for each type
+      // push request into array
       types.forEach(({ type }) => {
         axiosRequests.push(axios.get(type.url))
       })
@@ -42,14 +80,9 @@ export default function Weaknesses({ ...rest }) {
   }, [types])
 
   return (
-    <>
-      {pokemonInfo.isLoading ? (
-        <Loading />
-      ) : (
-        <Box align={{ sm: 'center', lg: 'flex-start' }} {...rest}>
-          <SectionTitle>Weaknesses</SectionTitle>
-        </Box>
-      )}
-    </>
+    <Box align={{ sm: 'center', lg: 'flex-start' }} {...rest}>
+      <SectionTitle>Weaknesses</SectionTitle>
+      {typeLoading ? <Loading /> : <></>}
+    </Box>
   )
 }

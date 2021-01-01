13106ebbbdb4ae709ea2550f4e6e89e528856592
Deohgu <dioggopsanttos@gmail.com>
2021-01-01 22:49:54 +0000

    ✨ Enabling use of useRef with Box.

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,37 +1,51 @@
 import { BoxWrapper } from './StyledBox'
 
-export default function Box({
-  align = 'center',
-  as = 'div',
-  background,
-  children,
-  constrained,
-  direction = 'column',
-  fill,
-  flexWrap = 'nowrap',
-  grow = true,
-  height,
-  justify = 'center',
-  sizes,
-  width = '100%',
-  ...rest
-}) {
-  return (
-    <BoxWrapper
-      alignProp={align}
-      as={as}
-      constrained={constrained}
-      fillProp={fill}
-      flexDirection={direction}
-      flexWrap={flexWrap}
-      growProp={grow}
-      heightProp={height}
-      justifyProp={justify}
-      sizesProp={sizes}
-      widthProp={width}
-      {...rest}
-    >
-      {children}
-    </BoxWrapper>
-  )
-}
+import { forwardRef } from 'React'
+
+import { useForwardedRef } from '../../helpers/ref'
+
+const Box = forwardRef(
+  (
+    {
+      align = 'center',
+      as = 'div',
+      background,
+      children,
+      constrained,
+      direction = 'column',
+      fill,
+      flexWrap = 'nowrap',
+      grow = true,
+      height,
+      justify = 'center',
+      sizes,
+      width = '100%',
+      ...rest
+    },
+    ref
+  ) => {
+    const boxRef = useForwardedRef(ref)
+
+    return (
+      <BoxWrapper
+        ref={boxRef}
+        alignProp={align}
+        as={as}
+        constrained={constrained}
+        fillProp={fill}
+        flexDirection={direction}
+        flexWrap={flexWrap}
+        growProp={grow}
+        heightProp={height}
+        justifyProp={justify}
+        sizesProp={sizes}
+        widthProp={width}
+        {...rest}
+      >
+        {children}
+      </BoxWrapper>
+    )
+  }
+)
+
+export default Box

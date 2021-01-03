b7bb2aa0f8ef3cf96251bc540add900530e7166d
andreferreiradlw <ferreirandre25@gmail.com>
2021-01-03 13:31:18 +0000

    Removes Github Pages from Deploy Strategy

--- src/pages/pokemon/[id].js
+++ src/pages/pokemon/[id].js
@@ -1,21 +1,3 @@
 import Pokemon from '../../components/Pokemon'
 
 export default Pokemon
-
-// This function gets called at build time
-export async function getStaticPaths() {
-  return {
-    // Only `/posts/1` and `/posts/2` are generated at build time
-    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
-    // Enable statically generating additional pages
-    // For example: `/posts/3`
-    fallback: true,
-  }
-}
-
-// This also gets called at build time
-export async function getStaticProps({ params }) {
-  return {
-    props: {}, // will be passed to the page component as props
-  }
-}

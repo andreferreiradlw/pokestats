0cb7f5a71d7371424b068d192dc710dfad17c0a0
andreferreiradlw <ferreirandre25@gmail.com>
2021-01-03 12:31:03 +0000

    Static Path generation for Pokemon Pages

--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -112,3 +112,19 @@ export default function Homepage() {
     </Layout>
   )
 }
+
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=3')
+  const pokemonList = await res.json()
+
+  // Get the paths we want to pre-render based on posts
+  const paths = pokemonList.results.map(pokemon => ({
+    params: { id: pokemon.name },
+  }))
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: true }
+}

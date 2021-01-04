15a0d2c13bc9ccf98b82987cc05b356b4504a895
andreferreiradlw <ferreirandre25@gmail.com>
2021-01-04 17:18:23 +0000

    Navigation Component & Global Styling Update

--- src/components/Pokemon/Navigation/StyledNavigation.js
+++ src/components/Pokemon/Navigation/StyledNavigation.js
@@ -1,11 +1,118 @@
 import styled, { css, keyframes } from 'styled-components'
+import Box from '../../Box'
 
-const NavBtn = styled.button`
+const Arrow = styled.span`
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-top: 45px solid transparent;
+  border-bottom: 45px solid transparent;
+  transition: all 0.15s ease-in-out;
+`
+
+const ArrowLeft = styled(Arrow)`
+  right: 0;
+  border-right: 10px solid black;
+  box-shadow: 10px 0 0 0 black, 10px 3px 0 0 black;
+`
+const ArrowRight = styled(Arrow)`
+  left: 0;
+  border-left: 10px solid black;
+  box-shadow: -10px 0 0 0 black, -10px 3px 0 0 black;
+`
+
+const BtnSpan = styled.span`
+  display: block;
+  position: relative;
+  height: 90px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 0.5rem;
+  color: white;
+
+  ${({ left }) =>
+    left &&
+    css`
+      float: left;
+    `}
+
   ${({ right }) =>
     right &&
     css`
-      margin-left: auto;
+      float: right;
+    `}
+
+  ${({ isTitle }) =>
+    isTitle &&
+    css`
+      background-color: black;
+      width: 120px;
+      max-width: 120px;
     `}
 `
 
-export { NavBtn }
+const PokemonImg = styled.img`
+  width: auto;
+  height: 90px;
+  transition: all 0.15s ease-in-out;
+`
+
+const BtnContainer = styled.div``
+
+const BtnAnchor = styled.a`
+  position: relative;
+  display: inline-block;
+  overflow: hidden;
+  border: 2px solid black;
+  font-weight: 600;
+
+  ${({ left }) =>
+    left &&
+    css`
+      border-radius: 4px 0 0 4px;
+      border-right: 1px solid white;
+    `}
+
+  ${({ right }) =>
+    right &&
+    css`
+      border-radius: 0 4px 4px 0;
+      border-left: 1px solid white;
+    `}
+
+  &::active {
+    height: 51px;
+  }
+
+  &:hover {
+    cursor: pointer;
+
+    & ${ArrowLeft} {
+      right: 10px;
+    }
+
+    & ${ArrowRight} {
+      left: 10px;
+    }
+
+    & ${PokemonImg} {
+      transform: scale(1.1);
+    }
+  }
+`
+
+const NumberID = styled.span`
+  font-size: 2rem;
+`
+
+export {
+  BtnContainer,
+  BtnAnchor,
+  BtnSpan,
+  ArrowLeft,
+  ArrowRight,
+  PokemonImg,
+  NumberID,
+}
--- src/components/Pokemon/Navigation/index.js
+++ src/components/Pokemon/Navigation/index.js
@@ -1,28 +1,70 @@
-import { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import Link from 'next/link'
+// helpers
+import { removeDash } from '../../../helpers/typography'
 // components
 import Box from '../../Box'
 // styles
-import { NavBtn } from './StyledNavigation'
+import {
+  BtnContainer,
+  BtnAnchor,
+  BtnSpan,
+  ArrowLeft,
+  ArrowRight,
+  PokemonImg,
+  NumberID,
+} from './StyledNavigation'
 
 export default function Navigation({ ...rest }) {
   // pokemon selector
   const pokemonInfo = useSelector(state => state.pokemon.info)
+  // pokemon array
+  const allPokemon = useSelector(state => state.home.pokemon)
+  // pokemon array length
   const pokemonLength = useSelector(state => state.home.pokemonLength)
   // data
   const { id } = pokemonInfo.data
 
   return (
-    <Box direction="row" justify="space-between" {...rest}>
+    <Box direction="row" justify={{ xxs: 'center', lg: 'flex-end' }} {...rest}>
       {id !== 1 && (
-        <Link as={`/pokemon/${id - 1}`} href="/pokemon/[id]">
-          <NavBtn>Previous</NavBtn>
+        <Link as={`/pokemon/${allPokemon[id - 2].name}`} href="/pokemon/[id]">
+          <BtnContainer>
+            <BtnAnchor left>
+              <BtnSpan isIcon left>
+                <PokemonImg
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                    id - 1
+                  }.png`}
+                />
+                <ArrowLeft />
+              </BtnSpan>
+              <BtnSpan isTitle right>
+                <NumberID>{`#${id - 1}`}</NumberID>
+                {removeDash(allPokemon[id - 2].name)}
+              </BtnSpan>
+            </BtnAnchor>
+          </BtnContainer>
         </Link>
       )}
       {id !== pokemonLength && (
-        <Link as={`/pokemon/${id + 1}`} href="/pokemon/[id]">
-          <NavBtn right>Next</NavBtn>
+        <Link as={`/pokemon/${allPokemon[id].name}`} href="/pokemon/[id]">
+          <BtnContainer>
+            <BtnAnchor right>
+              <BtnSpan isIcon right>
+                <PokemonImg
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                    id + 1
+                  }.png`}
+                />
+                <ArrowRight />
+              </BtnSpan>
+              <BtnSpan isTitle left>
+                <NumberID>{`#${id + 1}`}</NumberID>
+                {removeDash(allPokemon[id].name)}
+              </BtnSpan>
+            </BtnAnchor>
+          </BtnContainer>
         </Link>
       )}
     </Box>

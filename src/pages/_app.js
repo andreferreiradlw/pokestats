e1f1a519a80a3446362e406430af3408b3fb708c
andreferreiradlw <ferreirandre25@gmail.com>
2021-01-16 13:57:56 +0000

    Updates yarn dependencies

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,12 +9,8 @@ import ThemeProvider from '../components/Theme'
 import Head from '../components/Head'
 
 export default function App({ Component, pageProps }) {
-  // fetch initial pokemon list on app load
   useEffect(() => {
-    store.dispatch(fetchPokemonList())
-  }, [])
-
-  useEffect(() => {
+    // register service worker
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', function () {
         navigator.serviceWorker
@@ -23,14 +19,8 @@ export default function App({ Component, pageProps }) {
           .catch(err => console.dir(err))
       })
     }
-    /** 
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then(() => console.log('service worker registered.'))
-        .catch(err => console.dir(err))
-    }
-    */
+    // fetch initial pokemon list on app load
+    store.dispatch(fetchPokemonList())
   }, [])
 
   return (

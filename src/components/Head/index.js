2af1f258758d95ce6ba62d20e4427bc4001474d4
andreferreiradlw <ferreirandre25@gmail.com>
2021-01-18 12:50:50 +0000

    Updates Application Icons

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -26,33 +26,100 @@ export default function Heading() {
         name="keywords"
         content="pokemon, pokedex, pokestats, react, nextjs, styled-components, pokeapi"
       />
-      <title>{name && `${removeDash(name)} - `}PokeStats</title>
+      <title>{name && `${removeDash(name)} - `}PokeStats.gg</title>
       <link rel="canonical" href="https://pokestats.gg" />
       {/** MANIFEST */}
       <link href="/manifest.json" rel="manifest" />
       {/** ICONS */}
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/static/favicon.ico" />
       <link
-        href="/favicon-16x16.png"
+        href="/static/favicon-16x16.png"
         rel="icon"
         type="image/png"
         sizes="16x16"
       />
       <link
-        href="/favicon-32x32.png"
+        href="/static/favicon-32x32.png"
         rel="icon"
         type="image/png"
         sizes="32x32"
       />
       <link
         rel="apple-touch-icon"
+        sizes="57x57"
+        href="/static/apple-icon-57x57.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/static/apple-icon-60x60.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/static/apple-icon-72x72.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/static/apple-icon-76x76.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/static/apple-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/static/apple-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/static/apple-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/static/apple-icon-152x152.png"
+      />
+      <link
+        rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="/static/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/static/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/static/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/static/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/static/favicon-16x16.png"
       />
       {/** WINDOWS */}
       <meta name="msapplication-navbutton-color" content="red" />
       <meta name="msapplication-TileColor" content="blue" />
-      <meta name="msapplication-TileImage" content="mstile-150x150.png" />
+      <meta
+        name="msapplication-TileImage"
+        content="/static/ms-icon-144x144.png"
+      />
       <meta name="msapplication-config" content="browserconfig.xml" />
       {/** ANDROID */}
       <meta name="theme-color" content="black" />
@@ -63,7 +130,10 @@ export default function Heading() {
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       {/** Pinned Sites */}
       <meta name="application-name" content="PokeStats" />
-      <meta name="msapplication-tooltip" content="Pin Me" />
+      <meta
+        name="msapplication-tooltip"
+        content="Online encyclopedia of Pokémon species."
+      />
       <meta name="msapplication-starturl" content="/" />
       {/** Tap highlighting */}
       <meta name="msapplication-tap-highlight" content="no" />

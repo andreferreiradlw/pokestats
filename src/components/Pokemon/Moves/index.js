c0e83b9c3f88f06202be067b9f015c12ef968307
andreferreiradlw <ferreirandre25@gmail.com>
2021-01-21 16:55:28 +0000

    Fixes bug in TM names

--- src/components/Pokemon/Moves/index.js
+++ src/components/Pokemon/Moves/index.js
@@ -133,7 +133,11 @@ export default function Moves({ ...rest }) {
             // get machine names from responses
             const names = responses.map(res => {
               // if request fails will return null
-              res === null ? '❌' : res.data.item.name
+              if (res === null) {
+                return '❌'
+              } else {
+                return res.data.item.name
+              }
             })
             // update machine names state
             setMachineNames(names)

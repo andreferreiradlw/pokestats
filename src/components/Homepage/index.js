a8d2fc75e1dd44ccc91c0b5c6f450a81b999c711
andreferreiradlw <ferreirandre25@gmail.com>
2021-01-21 16:24:04 +0000

    Manage Homepage Loading State

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,5 +1,8 @@
-import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import LazyLoad from 'react-lazyload'
+// actions
+import { startLoading, stopLoading } from './homeSlice'
 // components
 import Layout from '../Layout'
 import Autocomplete from '../Autocomplete'
@@ -11,13 +14,25 @@ import { Container } from './styledHomepage'
 import { MainHeading } from '../BaseStyles'
 
 export default function Homepage() {
+  // dispatch
+  const dispatch = useDispatch()
+  // redux state
   const homeState = useSelector(state => state.home)
-
+  // data
   const { isLoading, pokemonLength } = homeState
+
+  // start loading again when unmounts
+  useEffect(() => {
+    if (pokemonLength) dispatch(stopLoading())
+    // on unmount
+    return () => {
+      dispatch(startLoading())
+    }
+  }, [])
 
   return (
     <>
-      {isLoading && !pokemonLength ? (
+      {isLoading || pokemonLength === 0 ? (
         <Loading />
       ) : (
         <>

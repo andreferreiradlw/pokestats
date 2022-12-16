62546901b088915250c1a25112a97ea7baa53c30
andreferreiradlw <ferreirandre25@gmail.com>
2022-12-16 14:32:30 +0000

    refactors Type page component

---
+++ src/components/Type/index.tsx
@@ -0,0 +1,82 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useRouter } from 'next/router';
+import { AnimatePresence } from 'framer-motion';
+// types
+import { PokestatsTypePageProps } from '@/pages/type/[typeId]';
+// actions
+import { fetchTypeData, cleanData } from './typeSlice';
+// helpers
+import { typeList, removeDash, pageContainerVariant } from '../../helpers';
+// components
+import Layout, { MainContainer } from '../Layout';
+import Loading from '../Loading';
+import Box from '../Box';
+import TypeInfo from './Info';
+import TypeRelations from './Relations';
+import TypeIcon from './TypeIcon';
+import Tabs from './Tabs';
+// styles
+import { PageHeading } from '../BaseStyles';
+
+const TypePage = ({ typeInfo, typeMoves }: PokestatsTypePageProps): JSX.Element => {
+  console.log('typeInfo', typeInfo);
+  console.log('typeMoves', typeMoves);
+  // data
+  const { name, names, damage_relations } = typeInfo;
+
+  return (
+    <Layout withHeader withFooter={true} withMain={false} withGameVersion={false} key="layout-type">
+      <AnimatePresence mode="wait">
+        <MainContainer
+          justify="flex-start"
+          align="flex-start"
+          $constrained
+          $withGutter
+          initial="hidden"
+          animate="visible"
+          exit="fade"
+          variants={pageContainerVariant}
+          key={`type-${name}`}
+        >
+          <Box
+            direction={{ xxs: 'column-reverse', lg: 'row' }}
+            align="flex-start"
+            justify="flex-start"
+            margin="1rem 0"
+          >
+            <Box
+              justify={{ xxs: 'center', lg: 'flex-start' }}
+              align={{ xxs: 'center', lg: 'flex-start' }}
+            >
+              <PageHeading>{removeDash(name)}</PageHeading>
+              <Box
+                direction={{ xxs: 'column', md: 'row' }}
+                justify={{ xxs: 'center', md: 'flex-start' }}
+                align={{ xxs: 'center', md: 'flex-start' }}
+                sizes={{ xxs: 12, lg: 8 }}
+              >
+                {/* <TypeInfo
+                  margin={{ xxs: '0 0 2rem', lg: '0' }}
+                  padding={{ xxs: '0', md: '0 1rem 0 0' }}
+                  info={typeInfo.data}
+                /> */}
+                {/* <TypeRelations
+                  margin={{ xxs: '0 0 2rem', lg: '0' }}
+                  padding={{ xxs: '0', md: '0 0 0 1rem', lg: '0 1rem' }}
+                  relations={damage_relations}
+                /> */}
+              </Box>
+            </Box>
+            <TypeIcon sizes={{ xxs: 12, lg: 4 }} typeName={name} otherNames={names} />
+          </Box>
+          <Box align="flex-start" justify="flex-start" margin="1rem 0">
+            {/* <Tabs sizes={12} margin="0 0 2rem" /> */}
+          </Box>
+        </MainContainer>
+      </AnimatePresence>
+    </Layout>
+  );
+};
+
+export default TypePage;
---
+++ src/pages/type/[typeId].tsx
@@ -0,0 +1,88 @@
+// types
+import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+// helpers
+import { PokemonClient, MoveClient, Type, Move } from 'pokenode-ts';
+import { getIdFromMove, getIdFromPokemon } from '@/helpers';
+// components
+import TypePage from '@/components/Type';
+
+export interface PokestatsTypePageProps {
+  typeInfo: Type;
+  typeMoves: Move[];
+}
+
+const PokestatsTypePage: NextPage<PokestatsTypePageProps> = ({ ...props }) => {
+  return <TypePage {...props} />;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // clients
+  const pokemonClient = new PokemonClient();
+
+  const typeList = await pokemonClient.listTypes();
+
+  const paths = typeList.results.map(type => {
+    return {
+      params: {
+        typeId: type.name,
+      },
+    };
+  });
+  // return static paths
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  // clients
+  const pokemonClient = new PokemonClient();
+  const moveClient = new MoveClient();
+
+  const typeName = params.typeId as string;
+  try {
+    const typeData: Type = await pokemonClient.getTypeByName(typeName);
+
+    if (!typeData) {
+      console.error('Failed to fetch typeData');
+      return { notFound: true };
+    }
+
+    // move requests array
+    let moveRequests = [];
+    // create an axios request for each move
+    typeData.moves.forEach(({ url }) =>
+      moveRequests.push(moveClient.getMoveById(getIdFromMove(url))),
+    );
+
+    const allPokemonMovesData: Move[] = await Promise.all(moveRequests);
+
+    const pokemonListWithId = typeData.pokemon
+      .map(({ pokemon }) => {
+        const id = getIdFromPokemon(pokemon.url);
+        // if pokemon not gen 8
+        if (id <= 809) {
+          return {
+            ...pokemon,
+            id: id,
+          };
+        }
+        return null;
+      })
+      .filter(Boolean);
+
+    return {
+      props: {
+        typeInfo: { ...typeData, pokemon: pokemonListWithId },
+        typeMoves: allPokemonMovesData,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    // redirects to 404 page
+    return { notFound: true };
+  }
+};
+
+export default PokestatsTypePage;

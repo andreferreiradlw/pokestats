bfaafc99074807c3e2dbd439cac1d39bd3de0248
andreferreiradlw <ferreirandre25@gmail.com>
2023-01-10 17:15:37 +0000

    memoizes Pokemon Details component

--- src/components/Pokemon/Details/index.tsx
+++ src/components/Pokemon/Details/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 // types
 import type { BoxProps } from '@/components/Box';
 import type { PokestatsPokemonPageProps } from '@/pages/pokemon/[pokemonId]';
@@ -9,61 +9,11 @@ import { AnimatePresence } from 'framer-motion';
 import { capitalize, removeDash } from '@/helpers/typography';
 import { fadeInUpVariant } from '@/helpers/animations';
 // components
-import Box from '@/components/Box';
 import BoxWrapper from '@/components/Box/StyledBox';
 import TypeBadge from '@/components/TypeBadge';
 // styles
 import { PageHeading, Table, Numbered } from '@/components/BaseStyles';
 import { TypeContainer, Genera, Flavor } from './StyledDetails';
-
-// flavor text
-const flavorText = (version: string, textEntries: FlavorText[]) => {
-  const versionEntry = textEntries.filter(entry => {
-    // @ts-ignore
-    return entry.version.name === version;
-  });
-  // return formatted text
-  // page breaks are treated just like newlines
-  // soft hyphens followed by newlines vanish
-  // letter-hyphen-newline becomes letter-hyphen, to preserve real hyphenation
-  // any other newline becomes a space
-  return versionEntry.length
-    ? versionEntry[0].flavor_text
-        .replace(/\u00AD/g, '')
-        .replace(/\u000C/g, ' ')
-        .replace(/u' -\n'/, ' - ')
-        .replace(/u'-\n'/, '-')
-        .replace(/(\r\n|\n|\r)/gm, ' ')
-    : 'No description available for currently selected generation.';
-};
-
-// weight
-const pokemonWeight = (currWeight: number): string =>
-  `${currWeight / 10} kg ( ${Math.round(currWeight * 2.2046) / 10} lbs )`;
-
-// height
-const pokemonHeight = (currHeight: number): string => {
-  // calculate height in feet
-  const heightInFeet = Math.round(currHeight * 3.2808) / 10;
-  // split number
-  const numbers = heightInFeet.toString().split('.');
-  // return string
-  return `${currHeight / 10} m ( ${numbers[0] || '0'}'${numbers[1] || '0'}" )`;
-};
-
-// abilities
-const renderAbilities = (
-  currAbilities: PokemonAbility[],
-  abilitiesData: Ability[],
-): JSX.Element[] =>
-  currAbilities.map(({ ability, is_hidden }, i) => (
-    <Numbered key={`${ability}-${i}`}>
-      {`${i + 1}. ${removeDash(ability.name)}`}
-      {is_hidden && ' ( Hidden Ability )'}
-      <br />
-      <span>{abilitiesData[i].effect_entries[0]?.short_effect}</span>
-    </Numbered>
-  ));
 
 interface PokemonDetailsProps extends BoxProps {
   pokemon: PokestatsPokemonPageProps['pokemon'];
@@ -82,6 +32,51 @@ const PokemonDetails = ({
   // data
   const { types, abilities: pokemonAbilities, id, name, weight, height } = pokemon;
   const { genera, flavor_text_entries, shape, color, is_baby, is_legendary, is_mythical } = species;
+  // memo
+  const flavorText = useMemo(() => {
+    // @ts-ignore
+    const versionEntry = flavor_text_entries.filter(entry => entry.version.name === gameVersion);
+    // return formatted text
+    // page breaks are treated just like newlines
+    // soft hyphens followed by newlines vanish
+    // letter-hyphen-newline becomes letter-hyphen, to preserve real hyphenation
+    // any other newline becomes a space
+    return versionEntry.length
+      ? versionEntry[0].flavor_text
+          .replace(/\u00AD/g, '')
+          .replace(/\u000C/g, ' ')
+          .replace(/u' -\n'/, ' - ')
+          .replace(/u'-\n'/, '-')
+          .replace(/(\r\n|\n|\r)/gm, ' ')
+      : 'No description available for currently selected generation.';
+  }, [gameVersion, flavor_text_entries]);
+
+  const pokemonWeight = useMemo(
+    () => `${weight / 10} kg ( ${Math.round(weight * 2.2046) / 10} lbs )`,
+    [weight],
+  );
+
+  const pokemonHeight = useMemo(() => {
+    // calculate height in feet
+    const heightInFeet = Math.round(height * 3.2808) / 10;
+    // split number
+    const numbers = heightInFeet.toString().split('.');
+    // return string
+    return `${height / 10} m ( ${numbers[0] || '0'}'${numbers[1] || '0'}" )`;
+  }, [height]);
+
+  const renderAbilities = useMemo(
+    () =>
+      pokemonAbilities.map(({ ability, is_hidden }, i) => (
+        <Numbered key={`${ability}-${i}`}>
+          {`${i + 1}. ${removeDash(ability.name)}`}
+          {is_hidden && ' ( Hidden Ability )'}
+          <br />
+          <span>{abilities[i].effect_entries[0]?.short_effect}</span>
+        </Numbered>
+      )),
+    [pokemonAbilities, abilities],
+  );
 
   return (
     <AnimatePresence mode="wait">
@@ -112,7 +107,7 @@ const PokemonDetails = ({
             Pokemon
           </Genera>
         )}
-        <Flavor>{flavorText(gameVersion, flavor_text_entries)}</Flavor>
+        <Flavor>{flavorText}</Flavor>
         <Table forwardedAs="table">
           <tbody>
             <tr>
@@ -125,15 +120,15 @@ const PokemonDetails = ({
             </tr>
             <tr>
               <th>Weight</th>
-              <td>{pokemonWeight(weight)}</td>
+              <td>{pokemonWeight}</td>
             </tr>
             <tr>
               <th>Height</th>
-              <td>{pokemonHeight(height)}</td>
+              <td>{pokemonHeight}</td>
             </tr>
             <tr>
               <th>Abilities</th>
-              <td>{renderAbilities(pokemonAbilities, abilities)}</td>
+              <td>{renderAbilities}</td>
             </tr>
             <tr>
               <th>Shape</th>

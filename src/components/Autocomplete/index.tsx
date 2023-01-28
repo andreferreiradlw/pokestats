d93aba2c6a1c3e85412b4314b89003371f4112c8
andreferreiradlw <ferreirandre25@gmail.com>
2023-01-28 18:02:08 +0000

    improvements to Autocomplete and InfiniteScroll

--- src/components/Autocomplete/index.tsx
+++ src/components/Autocomplete/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useState, useEffect, CSSProperties } from 'react';
+import { useState, useEffect, CSSProperties, useCallback } from 'react';
 // helpers
 import { removeDash, prefixId, fadeInDownVariant } from '@/helpers';
 // types
@@ -25,6 +25,35 @@ export interface AutocompleteProps extends HTMLMotionProps<'div'> {
   filterList: (PokemonType | Pokemon | MoveType)[];
   width?: CSSProperties['width'];
 }
+
+interface AutocompleteIconProps {
+  assetType: PokemonType['assetType'] | Pokemon['assetType'] | MoveType['assetType'];
+  name: PokemonType['name'];
+  id?: Pokemon['id'];
+}
+
+const AutocompleteIcon = ({ assetType, name, id }: AutocompleteIconProps): JSX.Element => {
+  switch (assetType) {
+    case 'pokemon':
+      return (
+        <PokemonImg
+          alt={`${name} pokemon`}
+          src={`https://raw.githubusercontent.com/andreferreiradlw/pokestats_media/main/assets/images/${prefixId(
+            id,
+          )}.png`}
+        />
+      );
+    case 'type':
+      return <TypeIcon type={name} />;
+    case 'move':
+      return (
+        <MachineImg
+          alt={`${name} pokemon move`}
+          src="https://raw.githubusercontent.com/msikma/pokesprite/master/items/hm/normal.png"
+        />
+      );
+  }
+};
 
 const Autocomplete = ({ filterList, ...rest }: AutocompleteProps): JSX.Element => {
   // router
@@ -56,59 +85,62 @@ const Autocomplete = ({ filterList, ...rest }: AutocompleteProps): JSX.Element =
     setSearch(e.target.value);
     handleFilter(e.target.value.toLowerCase());
   };
-
-  // filter by option
-  const handleFilter = (value: string): void => {
-    if (value) {
-      const filteredList = filterList.filter(
-        item =>
-          removeDash(item.name).toLowerCase().includes(value) ||
-          item.id.toString().includes(value.toString()),
-      );
-      // update filtered state with first 4 options
-      setFiltered(filteredList.slice(0, 4));
-    } else {
-      // set filtered state to empty array
-      setFiltered([]);
-    }
-  };
-
-  // key pressed
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement | HTMLAnchorElement>): void => {
-    // enter
-    if (e.key === 'Enter' && filtered[0] !== undefined) {
-      activeOption === -1
-        ? // trigger router for first suggestion
-          router.push(`/${filtered[0].assetType}/${filtered[0].name}`)
-        : // trigger router for active option
-          router.push(`/${filtered[activeOption].assetType}/${filtered[activeOption].name}`);
-      // clean filtered state
-      resetStates();
-    } // up arrow
-    else if (e.key === 'ArrowUp') {
-      // stop window from scrolling
-      e.preventDefault();
-      if (activeOption === -1) {
-        return;
+  // memo
+  const handleFilter = useCallback(
+    (value: string): void => {
+      if (value) {
+        const filteredList = filterList.filter(
+          item =>
+            removeDash(item.name).toLowerCase().includes(value) ||
+            item.id.toString().includes(value.toString()),
+        );
+        // update filtered state with first 4 options
+        setFiltered(filteredList.slice(0, 4));
+      } else {
+        // set filtered state to empty array
+        setFiltered([]);
       }
-      // decrement the index
-      setActiveOption(activeOption - 1);
-    }
-    // down arrow
-    else if (e.key === 'ArrowDown') {
-      // stop window from scrolling
-      e.preventDefault();
-      if (activeOption + 1 === filtered.length) {
-        // last option, do nothing
-        return;
+    },
+    [filterList],
+  );
+  const handleKeyDown = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement | HTMLAnchorElement>): void => {
+      // enter
+      if (e.key === 'Enter' && filtered[0] !== undefined) {
+        activeOption === -1
+          ? // trigger router for first suggestion
+            router.push(`/${filtered[0].assetType}/${filtered[0].name}`)
+          : // trigger router for active option
+            router.push(`/${filtered[activeOption].assetType}/${filtered[activeOption].name}`);
+        // clean filtered state
+        resetStates();
+      } // up arrow
+      else if (e.key === 'ArrowUp') {
+        // stop window from scrolling
+        e.preventDefault();
+        if (activeOption === -1) {
+          return;
+        }
+        // decrement the index
+        setActiveOption(activeOption - 1);
       }
-      // increment the index
-      setActiveOption(activeOption + 1);
-    } else {
-      // reset active option
-      setActiveOption(-1);
-    }
-  };
+      // down arrow
+      else if (e.key === 'ArrowDown') {
+        // stop window from scrolling
+        e.preventDefault();
+        if (activeOption + 1 === filtered.length) {
+          // last option, do nothing
+          return;
+        }
+        // increment the index
+        setActiveOption(activeOption + 1);
+      } else {
+        // reset active option
+        setActiveOption(-1);
+      }
+    },
+    [activeOption, filtered, router],
+  );
 
   return (
     <Container {...rest}>
@@ -146,21 +178,7 @@ const Autocomplete = ({ filterList, ...rest }: AutocompleteProps): JSX.Element =
                 ref={currOption => currOption && i === activeOption && currOption.focus()}
               >
                 <Box flexdirection="row" flexjustify="flex-start" flexgap="1em">
-                  {assetType === 'type' && <TypeIcon type={name} />}
-                  {assetType === 'pokemon' && (
-                    <PokemonImg
-                      alt={`${name} pokemon`}
-                      src={`https://raw.githubusercontent.com/andreferreiradlw/pokestats_media/main/assets/images/${prefixId(
-                        id,
-                      )}.png`}
-                    />
-                  )}
-                  {assetType === 'move' && (
-                    <MachineImg
-                      alt={`${name} pokemon move`}
-                      src="https://raw.githubusercontent.com/msikma/pokesprite/master/items-outline/tm/ghost.png"
-                    />
-                  )}
+                  <AutocompleteIcon assetType={assetType} name={name} id={id} />
                   <Option>{removeDash(name)}</Option>
                 </Box>
                 {assetType === 'pokemon' && <PokeID>{`#${id}`}</PokeID>}

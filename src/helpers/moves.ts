ca6f16dd97fa79dc0775052f0dc935acd1e79475
andreferreiradlw <ferreirandre25@gmail.com>
2024-08-12 15:41:16 +0100

    removes redundant moves helpers

--- src/helpers/moves.ts
+++ src/helpers/moves.ts
@@ -1,7 +1,5 @@
-import type { MoveLearnMethod, Machine, NamedAPIResource } from 'pokenode-ts';
+import type { MoveLearnMethod, NamedAPIResource } from 'pokenode-ts';
 import type { PokemonMove } from '@/types';
-import { MachineClient } from 'pokenode-ts';
-import { getIdFromMachine } from '@/helpers';
 
 export interface FilteredMove extends PokemonMove {
   level_learned_at: number;
@@ -12,51 +10,36 @@ const filterMoves = (
   moves: PokemonMove[],
   learnMethod: MoveLearnMethod['name'],
   versionGroup: string,
-): FilteredMove[] => {
-  return moves
-    .filter(move =>
-      move.version_group_details.some(
+): FilteredMove[] =>
+  moves
+    .map((move): FilteredMove | null => {
+      const group = move.version_group_details.find(
         group =>
           group.version_group.name === versionGroup && group.move_learn_method.name === learnMethod,
-      ),
-    )
-    .map(move => {
-      const group = move.version_group_details.find(
-        group => group.version_group.name === versionGroup,
       );
 
+      if (!group) return null;
+
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      const filteredMove: FilteredMove = { ...move } as FilteredMove;
+
       if (learnMethod === 'level-up') {
-        (move as FilteredMove).level_learned_at = group?.level_learned_at || 0;
+        filteredMove.level_learned_at = group.level_learned_at || 0;
       }
 
       if (learnMethod === 'machine') {
         const machine = move.machines?.find(machine => machine.version_group.name === versionGroup);
-        (move as FilteredMove).current_version_machine = machine?.machine.url;
+        filteredMove.current_version_machine = machine?.machine.url;
       }
 
-      return move as FilteredMove;
+      return filteredMove;
     })
-    .sort(
-      learnMethod === 'level-up'
-        ? (a, b) => (a as FilteredMove).level_learned_at - (b as FilteredMove).level_learned_at
-        : undefined,
-    );
-};
-
-const getMachineNames = async (machineMoves: FilteredMove[]): Promise<string[]> => {
-  const machineClient = new MachineClient();
-  const machineRequests = machineMoves
-    .filter(move => move?.current_version_machine)
-    .map(move => machineClient.getMachineById(getIdFromMachine(move.current_version_machine!)));
-
-  const machines = await Promise.all(machineRequests);
-
-  return machines.map(machine => machine.item.name);
-};
+    .filter((move): move is FilteredMove => move !== null)
+    .sort((a, b) => (learnMethod === 'level-up' ? a.level_learned_at - b.level_learned_at : 0));
 
 const removeDuplicateMoves = (moves: NamedAPIResource[]): NamedAPIResource[] =>
   Array.from(new Set(moves.map(move => move.name))).map(
     name => moves.find(move => move.name === name)!,
   );
 
-export { filterMoves, getMachineNames, removeDuplicateMoves };
+export { filterMoves, removeDuplicateMoves };

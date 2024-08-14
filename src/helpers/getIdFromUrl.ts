7f45e030c94ebed40f40366bbd7d880c433a62f6
andreferreiradlw <ferreirandre25@gmail.com>
2024-08-14 13:46:49 +0100

    creates getResourceId helper fn

--- src/helpers/getIdFromUrl.ts
+++ src/helpers/getIdFromUrl.ts
@@ -34,6 +34,17 @@ const getIdFromURL = (url: string, matcher: string): number => {
   return Number(matchId?.[1]);
 };
 
+/**
+ * Retrieves the resource ID from a given URL.
+ *
+ * @param url - The URL from which to extract the resource ID.
+ * @return The extracted resource ID.
+ */
+const getResourceId = (url: string) => {
+  const matchResult = url.match(/\/(\d+)\/$/);
+  return Number(matchResult![1]);
+};
+
 export {
   getIdFromEvolutionChain,
   getIdFromMachine,
@@ -42,4 +53,5 @@ export {
   getIdFromPokemon,
   getIdFromURL,
   getIdFromItem,
+  getResourceId,
 };

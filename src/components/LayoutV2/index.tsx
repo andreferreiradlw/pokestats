881e50510226080312762d6ba42637e0216c159d
andreferreiradlw <ferreirandre25@gmail.com>
2024-08-22 17:42:56 +0100

    implements HeaderV2 in LayoutV2 component

--- src/components/LayoutV2/index.tsx
+++ src/components/LayoutV2/index.tsx
@@ -1,23 +1,30 @@
-// styles
-import { Stack, StackProps } from '@mui/material';
-import { LayoutContainer, ScrollButton } from './StyledLayoutV2';
+// hooks
+import { useIsClient, useWindowSize } from 'usehooks-ts';
+// helpers
+import { fadeInOutUpVariant, pageContainerVariant, scrollToTop } from '@/helpers';
+// components
+import { Stack, StackProps, useScrollTrigger } from '@mui/material';
 import { AnimatePresence, motion, HTMLMotionProps } from 'framer-motion';
 import Footer from '../Footer';
-import { fadeInOutUpVariant, pageContainerVariant, scrollToTop } from '@/helpers';
-import { useIsClient, useWindowSize } from 'usehooks-ts';
-import { useScrollPosition } from '@/hooks';
+import HeaderV2, { HeaderV2Props } from '../HeaderV2';
+// styles
+import { LayoutContainer, ScrollButton } from './StyledLayoutV2';
 // icons
 import ChevronTop from 'public/static/iconLibrary/chevron_top.svg';
 
 interface LayoutV2Props extends Omit<HTMLMotionProps<'main'>, keyof StackProps>, StackProps {
   withHeader?: boolean;
+  showGenSelect?: HeaderV2Props['showGenSelect'];
 }
 
-const LayoutV2 = ({ children, withHeader, ...rest }: LayoutV2Props): JSX.Element => {
+const LayoutV2 = ({ children, withHeader, showGenSelect, ...rest }: LayoutV2Props): JSX.Element => {
   // hooks
   const isClient = useIsClient();
   const { width } = useWindowSize();
-  const scrollPosition = useScrollPosition();
+  const scrollPosition = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 1000,
+  });
 
   return (
     <LayoutContainer maxWidth={false} disableGutters>
@@ -34,10 +41,11 @@ const LayoutV2 = ({ children, withHeader, ...rest }: LayoutV2Props): JSX.Element
           key="layout-grid-container"
           {...rest}
         >
+          {withHeader && <HeaderV2 showGenSelect={showGenSelect} />}
           {children}
           <Footer />
         </Stack>
-        {width > 768 && scrollPosition > 1000 && (
+        {width > 768 && scrollPosition && (
           <ScrollButton
             onClick={isClient && scrollToTop}
             whileHover="hover"

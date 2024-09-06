63d966d6a790291812829c8345e888fb014f1fd1
andreferreiradlw <ferreirandre25@gmail.com>
2024-09-06 11:26:36 +0100

    updates usePokemonEncounters hook logic

--- src/hooks/usePokemonEncounters.ts
+++ src/hooks/usePokemonEncounters.ts
@@ -1,13 +1,57 @@
-import { EncountersApi } from '@/services';
+import { EncountersApi, LocationAreaApi } from '@/services';
 import { useQuery, type UseQueryOptions, type UseQueryResult } from '@tanstack/react-query';
-import type { LocationAreaEncounter } from 'pokenode-ts';
+import type { LocationArea, NamedAPIResource, VersionEncounterDetail } from 'pokenode-ts';
+
+interface Encounters {
+  location_area: LocationArea;
+  version_details: VersionEncounterDetail;
+}
 
 export const usePokemonEncounters = (
   id: number,
-  options?: Partial<UseQueryOptions<LocationAreaEncounter[]>>,
-): UseQueryResult<LocationAreaEncounter[]> =>
-  useQuery<LocationAreaEncounter[]>({
-    queryKey: ['pokemonEncounters', id],
-    queryFn: async () => await EncountersApi.getById(id),
+  gameVersion: string,
+  options?: Partial<UseQueryOptions<Encounters[]>>,
+): UseQueryResult<Encounters[]> =>
+  useQuery<Encounters[]>({
+    queryKey: ['pokemonEncounters', id, gameVersion],
+    queryFn: async () => {
+      const encounters = await EncountersApi.getById(id);
+
+      // Filter relevant encounters based on the game version
+      const filteredEncounters = encounters.reduce<
+        { location_area: NamedAPIResource; version_details: VersionEncounterDetail }[]
+      >((acc, area) => {
+        const currVersionDetails = area.version_details.find(
+          details => details.version.name === gameVersion,
+        );
+
+        if (currVersionDetails) {
+          acc.push({
+            location_area: area.location_area,
+            version_details: currVersionDetails,
+          });
+        }
+
+        return acc;
+      }, []);
+
+      // If there are no relevant encounters, return early
+      if (filteredEncounters.length === 0) {
+        return [];
+      }
+
+      // Fetch location data in parallel
+      const locationData = await Promise.all(
+        filteredEncounters.map(({ location_area }) =>
+          LocationAreaApi.getByName(location_area.name),
+        ),
+      );
+
+      // Combine filtered encounters with their corresponding location data
+      return filteredEncounters.map(({ version_details }, index) => ({
+        location_area: locationData[index],
+        version_details,
+      }));
+    },
     ...options,
   });

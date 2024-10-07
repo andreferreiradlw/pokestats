eb96cd6855dc4c227838be8c0b7af20162b22c70
andreferreiradlw <ferreirandre25@gmail.com>
2024-10-07 21:42:00 +0100

    sitemap adjustments

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,18 +2,15 @@ import type { MetadataRoute } from 'next';
 // helpers
 import { fetchSitemapData } from '@/helpers';
 
-const toSitemapEntry = (host: string, route: string, priority = 1.0): MetadataRoute.Sitemap[0] => ({
+const toSitemapEntry = (host: string, route: string): MetadataRoute.Sitemap[0] => ({
   url: `https://${host}${route}`,
-  lastModified: new Date(),
-  changeFrequency: 'weekly',
-  priority,
 });
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const host = process.env.NEXT_PUBLIC_ENV_VAR === 'prod_deployment' ? 'pokestats.gg' : 'localhost';
 
   // Static pages
-  const routes = ['', '/headbutt-tree-finder', '/items', '/berries', '/moves'];
+  const staticRoutes = ['', '/headbutt-tree-finder', '/items', '/berries', '/moves'];
 
   try {
     const {
@@ -31,16 +28,16 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
     // Create the array of sitemap entries
     const sitemapEntries: MetadataRoute.Sitemap = [
-      ...routes.map(route => toSitemapEntry(host, route)),
-      ...allRegionsData.map(region =>
-        toSitemapEntry(host, `/regions/${region.generation}/${region.name}`, 0.9),
-      ),
+      ...staticRoutes.map(route => toSitemapEntry(host, route)),
       ...allPokemonData.map(pokemon => toSitemapEntry(host, `/pokemon/${pokemon.name}`)),
-      ...allTypesData.map(type => toSitemapEntry(host, `/type/${type.name}`, 0.8)),
-      ...allMovesData.map(move => toSitemapEntry(host, `/move/${move.name}`, 0.9)),
-      ...allItemsData.map(item => toSitemapEntry(host, `/item/${item.name}`, 0.8)),
-      ...allEggGroupsData.map(item => toSitemapEntry(host, `/egg-group/${item.name}`, 0.8)),
-      ...allPokemonData.map(pokemon => toSitemapEntry(host, `/sprites/${pokemon.name}`, 0.8)),
+      ...allPokemonData.map(pokemon => toSitemapEntry(host, `/sprites/${pokemon.name}`)),
+      ...allTypesData.map(type => toSitemapEntry(host, `/type/${type.name}`)),
+      ...allMovesData.map(move => toSitemapEntry(host, `/move/${move.name}`)),
+      ...allItemsData.map(item => toSitemapEntry(host, `/item/${item.name}`)),
+      ...allEggGroupsData.map(item => toSitemapEntry(host, `/egg-group/${item.name}`)),
+      ...allRegionsData.map(region =>
+        toSitemapEntry(host, `/regions/${region.generation}/${region.name}`),
+      ),
     ];
 
     return sitemapEntries;
